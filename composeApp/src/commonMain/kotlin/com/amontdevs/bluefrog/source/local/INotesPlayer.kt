1c76f64fad5a3044282d3a5050dfe70a6c3632d5
Alexander M. <aleex1742@gmail.com>
2025-05-03 11:34:29 -0600

    CU-86a8bycze - Fix iOS Enable iOS version

--- composeApp/src/commonMain/kotlin/com/amontdevs/bluefrog/source/local/INotesPlayer.kt
+++ composeApp/src/commonMain/kotlin/com/amontdevs/bluefrog/source/local/INotesPlayer.kt
@@ -5,5 +5,5 @@ import com.amontdevs.bluefrog.domain.AbsoluteNote
 expect interface INotesPlayer {
     fun playSound(bytes: ByteArray)
     fun stopPlaying()
-    fun isPlaying()
+    fun isPlaying(): Boolean
 }
---
+++ composeApp/src/iosMain/kotlin/com/amontdevs/bluefrog/di/KoinHelper.kt
@@ -0,0 +1,15 @@
+package com.amontdevs.bluefrog.di
+
+import org.koin.core.context.startKoin
+
+fun initKoin(){
+    startKoin {
+        modules(
+            listOf(
+                iOSSourceModule,
+                repositoryModule,
+                viewModelModule
+            )
+        )
+    }
+}
